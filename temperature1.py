c6113422043b45bf9cb8b8c086f6dd951236fd29
tuf84490 <tuf84490@temple.edu>
2019-05-28 16:08:51 -0400

    added the polynomial regression by getting average of data and doing polynomial regression on that

--- temperature1.py
+++ temperature1.py
@@ -45,25 +45,50 @@ for i in range(len(data)):
     if dates[i] == 2:
         day3.append(data[i])
         time3.append(times[i])
-    
-x = time1
-y = day1
-fit1 = np.polyfit(x,y,5)
-fit_fn1 = np.poly1d(fit1)
 
-x2 = time2
-y2 = day2
-fit2 = np.polyfit(x2,y2,5)
-fit_fn2 = np.poly1d(fit2)
+xavg = []
+yavg = []
+largest = []
+L1 = len(time1)
+L2 = len(time2)
+L3 = len(time3)
+#determine which is largest
+if (L1 > L2) and (L1 > L3):
+   largest = L1
+elif (L2 > L1) and (L2 > L3):
+   largest = L2
+else:
+   largest = L3
 
-x3 = time3
-y3 = day3
-fit3 = np.polyfit(x3,y3,5)
-fit_fn3 = np.poly1d(fit3)
+#get the average at that point, if theres not an average accross all three, get the average across what is available
+for i in range(largest):
+    if(i>=L1 and i<L2 and i<L3):   #(time1 is none)change all these to use the lengths of each array L1 L2 L3 instead of index checks
+        xavg.append( (time2[i] + time3[i]) / 2 )
+        yavg.append( (day2[i] + day3[i]) / 2 )
+    elif(i>=L2 and i<L1 and i<L3): #time2 is none
+        xavg.append( (time1[i] + time3[i]) / 2 )
+        yavg.append( (day1[i] + day3[i]) / 2 )
+    elif(i>=L3 and i<L1 and i<L2): #time3 is none
+        xavg.append( (time1[i] + time2[i]) / 2 )
+        yavg.append( (day1[i] + day2[i]) / 2 )
+    elif(i>=L1 and i>=L2 and i<L3): #time1[i] is None and time2[i] is None
+        xavg.append(time3[i])
+        yavg.append(day3[i])
+    elif(i>=L2 and i>=L3 and i<L1): #(time2[i] is None and time3[i] is None)
+        xavg.append(time1[i])
+        yavg.append(day1[i])
+    elif(i>=L1 and i>=L3 and i<L2): #time1[i] is None and time3[i] is None
+        xavg.append(time2[i])
+        yavg.append(day2[i])
+    else:
+        xavg.append( (time1[i] + time2[i] + time3[i]) / 3 )
+        yavg.append( (day1[i] + day2[i] + day3[i]) / 3 )
 
-plt.plot(x,y, 'r^', x, fit_fn1(x), '--r')
-plt.plot(x2,y2, 'cD', x2, fit_fn2(x2), '--c')
-plt.plot(x3,y3, 'ks', x3, fit_fn3(x3), '--k')
+fit = np.polyfit(xavg,yavg,5)
+fit_fn = np.poly1d(fit)
+
+plt.plot(xavg,yavg, 'gx', xavg, fit_fn(xavg), '--g')
+
 p1 = plt.scatter(time1, day1, marker=markers[0], color=colors1[0], label='1', s = 30)
 p2 = plt.scatter(time2, day2, marker=markers[1], color=colors1[1], label='1', s = 30)
 p3 = plt.scatter(time3, day3, marker=markers[2], color=colors1[2], label='1', s = 30)

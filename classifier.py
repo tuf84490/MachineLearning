a24c1dcd7f84ab6c0b0b6475e53fbe636b7d866d
tuf84490 <tuf84490@temple.edu>
2019-07-05 20:44:02 -0400

    added ability to save models once run and started identifing audio files with these models

---
+++ classifier.py
@@ -0,0 +1,58 @@
+import pickle
+import os
+import numpy as np
+from tqdm import tqdm
+from scipy.io import wavfile
+from python_speech_features import mfcc
+from keras.models import load_model
+import pandas as pd
+from sklearn.metrics import accuracy_score
+
+def build_predictions(audio_dir):
+    y_true = []
+    y_pred = []
+    fn_prob = {}
+    print('extracting feature form audio')
+    for fn in tqdm(os.listdir(audio_dir)):
+        rate, wav = wavfile.read(os.path.join(audio_dir, fn))
+        label = fn2class[fn]
+        c = classes.index(label)
+        y_prob = []
+
+        for i in range(0, wav.shape[0]-config.step, config.step):
+            sample = wav[i:i+config.step]
+            x = mfcc(sample, rate, numcep=config.nfeat, nfilt=config.nfilt,nfft=config.nfft)
+            x = (x - config.min) / (config.max - config.min)
+            if(config.mode == 'time'):
+                x = np.expand_dims(x, axis=0)
+            y_hat = model.predict(x)
+            y_prob.append(y_hat)
+            y_pred.append(np.argmax(y_hat))
+            y_true.append(c)
+        fn_prob[fn] = np.mean(y_prob, axis=0).flatten()
+    return y_true, y_pred, fn_prob
+
+df = pd.read_csv("ESC-50-master/meta/esc50.csv")
+classes = list(np.unique(df.category))
+fn2class = dict(zip(df.filename, df.category))
+p_path = os.path.join('bin', 'ESC_50.p')
+
+with open(p_path, 'rb') as handle:
+    config = pickle.load(handle)
+
+model = load_model(config.model_path)
+
+y_true, y_pred, fn_prob = build_predictions('clean')
+acc_score = accuracy_score(y_true=y_true, y_pred=y_pred)
+
+y_probs = []
+for i, row in df.iterrows():
+    y_prob = fn_prob[row.filename]
+    y_probs.append(y_prob)
+    for c, p in zip(classes, y_prob):
+        df.at[i,c] = p
+
+y_pred = [classes[np.argmax(y)] for y in y_probs]
+df['y_pred'] = y_pred
+
+df.to_csv('predictions.csv', index=False)
---
+++ modeler.py
@@ -0,0 +1,144 @@
+from keras.layers import Conv2D, MaxPool2D, Flatten, LSTM
+from keras.layers import Dropout, Dense, TimeDistributed
+from keras.models import Sequential
+from keras.utils import to_categorical
+from sklearn.utils.class_weight import compute_class_weight
+from python_speech_features import mfcc, logfbank
+from tqdm import tqdm
+import os
+import pandas as pd
+import numpy as np
+from scipy.io import wavfile
+from keras.callbacks import ModelCheckpoint
+import matplotlib.pyplot as plt
+import librosa
+import pickle
+
+def check_data():
+    if os.path.isfile(config.p_path):
+        print('loading in model {}'.format(config.name))
+        with open(config.p_path, 'rb') as handle:
+            tmp = pickle.load(handle)
+            return tmp
+    else:
+        return None
+def build_rand_feat():
+    tmp = check_data()
+    if tmp:
+        return tmp.data[0], tmp.data[1]
+    X = []
+    y = []
+    _min, _max = float('inf'), -float('inf')
+    for _ in tqdm(range(n_samples)):
+        rand_class = np.random.choice(class_dist.index, p=prob_dist)
+        file = np.random.choice(df[df.category==rand_class].index)
+        rate, wav = wavfile.read('ESC-50-master/audio/' + file)
+        label = df.at[file, 'category']
+        rand_index = np.random.randint(0, wav.shape[0]-config.step)
+        sample = wav[rand_index:rand_index+config.step]
+        X_sample = mfcc(sample, rate, numcep=config.nfeat, nfilt=config.nfilt,nfft=config.nfft)
+        _min = min(np.amin(X_sample), _min)
+        _max = max(np.amax(X_sample), _max)
+        X.append(X_sample)
+        y.append(classes.index(label))
+    config.min = _min
+    config.max = _max
+    X,y = np.array(X), np.array(y)
+    X = (X-_min) / (_max - _min)
+    if config.mode == 'conv':
+        X = X.reshape(X.shape[0], X.shape[1], X.shape[2], 1)
+    elif(config.mode == 'time'):
+        X = X.reshape(X.shape[0], X.shape[1], X.shape[2])
+    y = to_categorical(y, num_classes=50)
+    config.data = (X,y)
+    with open(config.p_path, 'wb') as handle:
+        pickle.dump(config,handle,protocol=2)
+    return X,y
+
+def get_conv_model():
+    model = Sequential()
+    model.add(Conv2D(16, (3,3), activation='relu', strides=(1,1), padding='same', input_shape=input_shape))
+    model.add(Conv2D(32, (3,3), activation='relu', strides=(1,1), padding='same', input_shape=input_shape))
+    model.add(Conv2D(64, (3,3), activation='relu', strides=(1,1), padding='same', input_shape=input_shape))
+    model.add(Conv2D(128, (3,3), activation='relu', strides=(1,1), padding='same', input_shape=input_shape))
+    model.add(MaxPool2D((2,2)))
+    model.add(Dropout(0.5))
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(64, activation='relu'))
+    model.add(Dense(50, activation='relu'))
+    model.summary()
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+    return model
+
+def get_recurrent_model():
+    model = Sequential()
+    model.add(LSTM(128,return_sequences=True, input_shape=input_shape))
+    model.add(LSTM(128,return_sequences=True))
+    model.add(LSTM(128,return_sequences=True))
+    model.add(LSTM(128,return_sequences=True))
+    model.add(LSTM(128,return_sequences=True))
+    model.add(Dropout(0.5))
+    model.add(TimeDistributed(Dense(64, activation='relu')))
+    model.add(TimeDistributed(Dense(32, activation='relu')))
+    model.add(TimeDistributed(Dense(16, activation='relu')))
+    model.add(TimeDistributed(Dense(8, activation='relu')))
+    model.add(Flatten())
+    model.add(Dense(50, activation='softmax'))
+    model.summary()
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+    return model
+
+class Config:
+    def __init__(self, mode='time', nfilt=26, nfeat=13, nfft=1103, rate=44100, name='default'):
+        self.name = name
+        self.mode = mode
+        self.nfilt = nfilt
+        self.nfeat = nfeat
+        self.nfft = nfft
+        self.rate = rate
+        self.step = int(rate/10)
+        self.model_path = os.path.join('models', name + '.model')
+        self.p_path = os.path.join('bin', name + '.p')
+
+df = pd.read_csv("ESC-50-master/meta/esc50.csv")
+df = df.set_index('filename')
+
+#sample rate is 44100
+#read in the rate and signal of all the audio files
+#store the audio data in a dataframe. This dataframe contains all the audio data, their class, and how long the audio file is
+for f in df.index:
+    rate, signal = wavfile.read("clean/" + f)
+    df.at[f,'length'] = signal.shape[0]/rate
+    #print(rate)
+    #print(signal)
+
+#get a list of all possible classes as well as the distrobution of the number of the particular class found compared to the total number of audio events
+classes = list(np.unique(df.category))
+class_dist = df.groupby(['category'])['length'].mean()
+
+#set a large samplesize and choose a random class
+n_samples = int(df['length'].sum() / 0.4 )
+prob_dist = class_dist / class_dist.sum()
+#choices = np.random.choice(class_dist.index, p = prob_dist)
+#print(choices)
+
+config = Config(mode='time', name='ESC_50')
+
+if(config.mode == 'time'):
+    X, y = build_rand_feat()
+    y_flat = np.argmax(y,axis=1)
+    input_shape = (X.shape[1], X.shape[2])
+    model = get_recurrent_model()
+elif(config.mode == 'conv'):
+    X, y = build_rand_feat()
+    y_flat = np.argmax(y,axis=1)
+    input_shape = (X.shape[1], X.shape[2], 1)
+    model = get_conv_model()
+
+class_weight = compute_class_weight('balanced', np.unique(y_flat), y_flat)
+
+checkpoint = ModelCheckpoint(config.model_path, monitor='val_acc', verbose=1, mode='max',save_best_only=True, save_weights_only=False, period=1)
+
+model.fit(X, y, epochs=80, batch_size=16, shuffle=True, class_weight=class_weight, validation_split=0.1, callbacks=[checkpoint])
+model.save(config.model_path)
